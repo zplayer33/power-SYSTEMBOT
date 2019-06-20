a2e4761adac2cafda63200c083693484f47f11c9
zplayer33 <51510077+zplayer33@users.noreply.github.com>
2019-06-20 02:17:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const sql = require("sqlite");
 const dateFormat = require('dateformat'); 
 const pretty = require('pretty-ms') 
-const prefix = 'z';
+const prefix = '!';
 var table = require('table').table
 var ti={}  
 ,spee={}
@@ -35,7 +35,7 @@ var ti={}
 
 client.on('ready', function(){
     var ms = 60000 ;
-    var setGame = ['zhelp','Type zhelp'];
+    var setGame = ['!help','Type !help'];
     var i = -1;
     var j = 0;
     setInterval(function (){
